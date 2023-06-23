c7462a5a73a318fc36f57e314130e4e6bf3b5bff
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-06-23 15:48:28 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,11 @@
 from http.server import BaseHTTPRequestHandler
-import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+SQLALCHEMY_DATABASE_URL = "postgres://default:iTr5WyGdCo1p@ep-bitter-term-594574.eu-central-1.postgres.vercel-storage.com:5432/verceldb"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 import json
 
 
@@ -11,53 +17,4 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
         
-        
-        conn = psycopg2.connect(
-        host="ep-bitter-term-594574-pooler.eu-central-1.postgres.vercel-storage.com",
-        database="verceldb",
-        user="default",
-        password="iTr5WyGdCo1p"
-        
-        cursor = conn.cursor()
-)
-        
-        query = "SELECT timestamp, validateur, denom, amount FROM rewards;"
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        
-        data = {
-                "labels": [],
-                "datasets": []
-            }
-
-            # Dictionnaire pour stocker les données de chaque label
-            label_data = {}
-
-            for row in rows:
-                timestamp, validateur, denom, amount = row
-
-                # Ajouter le label si ce n'est pas déjà présent dans le dictionnaire
-                if timestamp not in label_data:
-                    label_data[timestamp] = {
-                        "data": [],
-                        "label": timestamp,
-                        "borderColor": "#000000",
-                        "backgroundColor": "#000000",
-                        "fill": False
-                    }
-
-                # Ajouter les données dans le dictionnaire correspondant au label
-                label_data[timestamp]["data"].append(amount)
-
-                # Ajouter le label s'il n'est pas déjà présent dans la liste des labels
-                if timestamp not in data["labels"]:
-                    data["labels"].append(timestamp)
-
-            # Ajouter les données de chaque label dans le dataset
-            for label, label_info in label_data.items():
-                data["datasets"].append(label_info)
-        
-        
-        cursor.close()
-        conn.close()
         return
