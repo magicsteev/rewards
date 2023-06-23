9b0ac0513b4d8e7dd5bfaf538446139a59b8ce51
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-06-23 16:03:23 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,8 +2,9 @@ from http.server import BaseHTTPRequestHandler
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import psycopg2
 import json
+
 
 
 class handler(BaseHTTPRequestHandler):
@@ -13,5 +14,14 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/plain')
         self.end_headers()
         self.wfile.write('Hello, world!'.encode('utf-8'))
-        
+        conn = psycopg2.connect(
+        host="ep-bitter-term-594574-pooler.eu-central-1.postgres.vercel-storage.com",
+        database="verceldb",
+        user="default",
+        password="iTr5WyGdCo1p"
+        )
+        # Création d'un curseur pour exécuter les requêtes SQL
+        cursor = conn.cursor()
+        cursor.close()
+        conn.close()
         return
