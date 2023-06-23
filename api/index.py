724e9af4afa601dfeff4d5e7accf1db4c738c373
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-06-23 16:01:12 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,10 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "postgres://default:iTr5WyGdCo1p@ep-bitter-term-594574.eu-central-1.postgres.vercel-storage.com:5432/verceldb"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
 import json
 
 
