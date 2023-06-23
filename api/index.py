b9452d49c3c04d1dec92c2ab8f6311a7e46564c4
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-06-23 16:06:22 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,10 +10,7 @@ import json
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+
         conn = psycopg2.connect(
         host="ep-bitter-term-594574-pooler.eu-central-1.postgres.vercel-storage.com",
         database="verceldb",
@@ -22,6 +19,46 @@ class handler(BaseHTTPRequestHandler):
         )
         # Création d'un curseur pour exécuter les requêtes SQL
         cursor = conn.cursor()
+        query = "SELECT timestamp, validateur, denom, amount FROM rewards"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        data = {
+            "labels": [],
+            "datasets": []
+        }
+
+        # Dictionnaire pour stocker les données de chaque label
+        label_data = {}
+
+        for row in rows:
+            timestamp, validateur, denom, amount = row
+
+            # Ajouter le label si ce n'est pas déjà présent dans le dictionnaire
+            if timestamp not in label_data:
+                label_data[timestamp] = {
+                    "data": [],
+                    "label": timestamp,
+                    "borderColor": "#000000",
+                    "backgroundColor": "#000000",
+                    "fill": False
+                }
+
+            # Ajouter les données dans le dictionnaire correspondant au label
+            label_data[timestamp]["data"].append(amount)
+
+            # Ajouter le label s'il n'est pas déjà présent dans la liste des labels
+            if timestamp not in data["labels"]:
+                data["labels"].append(timestamp)
+
+        # Ajouter les données de chaque label dans le dataset
+        for label, label_info in label_data.items():
+            data["datasets"].append(label_info)
         cursor.close()
         conn.close()
+        
+        json_data = json.dumps(data)
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write(json_data.encode('utf-8'))
         return
